9554ef9c9f6a706a30449bc7a81923c109a1936d
Chandan Rai <chank.raiii@gmail.com>
2021-10-06 23:40:59 +0530

    toggle icon is done

--- src/components/HomePage/SelectBike/sub-components/Pagination.js
+++ src/components/HomePage/SelectBike/sub-components/Pagination.js
@@ -50,36 +50,40 @@ export default function Pagination(props) {
   }, [brandState, modelState]);
 
   // Dash Svg icon
-  // const DashIcon = (props) => (
-  //   <SvgIcon {...props}>
-  //     {/* <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" /> */}
-  //     <path d="M0 8a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1z" />
-  //   </SvgIcon>
-  // );
-
-  const DashIcon = createSvgIcon(
-    <path d="M0 8a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1z" />,
-    "Dash"
+  const DashIcon = (props) => (
+    <SvgIcon viewBox="-4 0 24 18" {...props}>
+      {/* <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" /> */}
+      <path d="M0 8a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1z" />
+      {/* <path
+        d="M122 1H6C3.23858 1 1 3.23858 1 6C1 8.76142 3.23858 11 6 11H122C124.761 11 127 8.76142 127 6C127 3.23858 124.761 1 122 1Z"
+        fill="#E6E6E6"
+        stroke="black"
+      /> */}
+    </SvgIcon>
   );
+
+  // const DashIcon = createSvgIcon(
+  //   <path d="M0 8a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1z" />,
+  //   "Dash"
+  // );
 
   return (
     <>
       <Grid item xs={6} md={4} lg={6}>
         <IconButton
           aria-label="back"
-          onClick={() => {
-            setCurrentPage(currentPage - 1);
-          }}
+          onClick={() => setCurrentPage(currentPage - 1)}
           disabled={currentPage === 0}
+          className="icon-btn-color"
         >
           <ArrowBack />
         </IconButton>
+
         <IconButton
           aria-label="forward"
-          onClick={() => {
-            setCurrentPage(currentPage + 1);
-          }}
+          onClick={() => setCurrentPage(currentPage + 1)}
           disabled={currentPage === totalPage - 1}
+          className="icon-btn-color"
         >
           <ArrowForward />
         </IconButton>
@@ -88,19 +92,14 @@ export default function Pagination(props) {
       <Grid item xs={6} md={4} lg={6}>
         <Box className="dash-icon-box">
           {Array.from({ length: totalPage }, (_, index) => (
-            <Box
-              //   sx={{
-              //     "& >:hover": {
-              //       color: "red",
-              //       //   fontSize: "1.8rem",
-              //     },
-              //   }}
-              className="dash_buttons"
+            <IconButton
+              aria-label="dash_toggle"
               key={index}
-              onClick={() => {
-                setCurrentPage(index);
-              }}
-            ></Box>
+              onClick={() => setCurrentPage(index)}
+              className="icon-btn-color"
+            >
+              <DashIcon />
+            </IconButton>
           ))}
         </Box>
       </Grid>
